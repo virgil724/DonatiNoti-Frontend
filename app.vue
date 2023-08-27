b3774860bb67691764ed781f0932dfc32984b289
TANG CHENG LEE <TANGCHENGLEE@virgil246.com>
2023-08-28 02:06:28 +0800

    Add icon and SEOMeta

--- app.vue
+++ app.vue
@@ -1,9 +1,36 @@
 <template>
   <!-- <div class="m-4 bg-white"> -->
-    <!-- <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p> -->
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+  <!-- <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p> -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
   <!-- </div> -->
-
 </template>
+
+<script setup>
+useSeoMeta({
+  title: "超級感謝小工具",
+  description: "將歐富寶，綠界的抖內通知轉為OBS上可用的Superchat樣式",
+  ogTitle: "超級感謝小工具",
+  ogDescription: "將歐富寶，綠界的抖內通知轉為OBS上可用的Superchat樣式",
+  ogImage: `${useRequestURL().toString()}/favicon.ico`,
+  ogUrl: useRequestURL().toString(),
+  twitterTitle: "超級感謝小工具",
+  twitterDescription: "將歐富寶，綠界的抖內通知轉為OBS上可用的Superchat樣式",
+  twitterImage: `${useRequestURL().toString()}/favicon.ico`,
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "zh-tw",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.ico",
+    },
+  ],
+});
+</script>
